8ea030375959b8d6d911c69d0789bed959aacc4b
Josell <vibarjosell@gmail.com>
2023-08-06 13:43:39 +0800

    upload image

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
@@ -24,7 +27,7 @@ class UserController extends Controller
     {
         $editing = true;
         $tweets = $user->tweets()->paginate(5);
-        return view('users.show',compact('user','editing','tweets'));
+        return view('users.edit',compact('user','editing','tweets'));
     }
 
     /**
@@ -32,7 +35,26 @@ class UserController extends Controller
      */
     public function update(User $user)
     {
-        //
+        $validated = request()->validate([
+            'name' => 'required|min:3|max:40',
+            'bio' => 'nullable|min:1|max:255',
+            'image' => 'image'
+        ]);
+
+        if(request()->has('image')){
+            $imagePath = request()->file('image')->store('profile','public');
+            $validated['image'] = $imagePath;
+
+            /* Storage::disk('public')->delete($user->image); */
+        }
+
+        $user->update($validated);
+
+        return redirect()->route('profile');
     }
 
+
+    public function profile() {
+        return $this->show(auth()->user());
+    }
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.layout')
+
+@section('content')
+    <div class="container py-4">
+        <div class="row">
+            @include('includes.left-sidebar')
+            <div class="col-6">
+                @include('includes.success-message')
+                <div class="mt-3">
+                    @include('includes.user-edit-card')
+                </div>
+                @forelse ($tweets as $tweet)
+                    <div class="mt-3">
+                        @include('includes.tweet-card')
+                    </div>
+                @empty
+                    <p class="text-center mt-3">No Results Found.</p>
+                @endforelse
+
+                <div class="mt-3">
+                    {{ $tweets->withQueryString()->links() }}
+                </div>
+            </div>
+            <div class="col-3">
+                @include('includes.search-bar')
+                @include('includes.follow-box')
+            </div>
+        </div>
+    </div>
+@endsection
